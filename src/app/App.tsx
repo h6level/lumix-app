203328225a890911e4be887bfd6c856f7e46f839
h6level <h6level@gmail.com>
2022-01-07 00:25:38 +0800

    change style

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,6 @@
 import { MemoryRouter, Routes, Route } from 'react-router-dom';
 
+import '@scss/index.scss';
 import '@fontsource/open-sans';
 import '@fontsource/open-sans/500.css';
 import CoinPng from '@static/icon/coin.png';
@@ -10,17 +11,24 @@ import './App.scss';
 const Hello = () => {
   return (
     <div className="hello-page">
-      <img width="200px" alt="icon" src={IconSvg} />
-      <h1 style={{ color: '#FA6163' }}>Lumix-app</h1>
-      <h1 style={{ color: '#7253ED' }}>Limix-app</h1>
-      <h1 style={{ color: '#2196F3' }}>Limix-app</h1>
-      <h1 style={{ color: '#673AB7' }}>Limix-app</h1>
       <p>
         <span className="material-icons material-icons-outlined">settings</span>
       </p>
-      <p style={{ fontSize: '16px' }}>[webpack-dev-server] App updated. Recompiling...</p>
-      <p style={{ fontSize: '17px' }}>[webpack-dev-server] App updated. Recompiling...</p>
-      <p style={{ fontSize: '18px' }}>[webpack-dev-server] App updated. Recompiling...</p>
+      <p style={{ fontSize: '16px', color: '#FA6163' }}>
+        [webpack-dev-server] App updated. Recompiling...
+      </p>
+      <p style={{ fontSize: '17px', color: '#7253ED' }}>
+        [webpack-dev-server] App updated. Recompiling...
+      </p>
+      <p style={{ fontSize: '18px', color: '#2196F3' }}>
+        [webpack-dev-server] App updated. Recompiling...
+      </p>
+      <p style={{ fontSize: '19px', color: '#673AB7' }}>
+        [webpack-dev-server] App updated. Recompiling...
+      </p>
+      <p className="img11">
+        <img width="200px" alt="icon" src={IconSvg} />
+      </p>
       <p><img src={CoinPng} alt="coin" style={{ width: '100px' }} /></p>
       <p><img src={RocketPng} alt="rocket" style={{ width: '100px' }} /></p>
     </div>
